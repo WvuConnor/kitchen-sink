65daa3a08c46a23e61150774af075bf00a451ad6
WvuConnor <caw00006@mix.wvu.edu>
2023-02-21 14:27:05 -0500

    ERROR FIX

--- src/app/cards/card/card.component.ts
+++ src/app/cards/card/card.component.ts
@@ -1,3 +1,4 @@
+/*
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -22,3 +23,39 @@ export class CardComponent implements OnInit {
   }
 
 }
+
+export const mock_list = [
+  {
+      img: "https://www.nasa.gov/sites/default/files/thumbnails/image/hubble_donotielloii_potw2306a.jpg",
+      description: "yo wuddup",
+      price: 99.99,
+      imgDescription: "galaxy 2"
+  },
+  {
+      img: "https://www.nasa.gov/sites/default/files/thumbnails/image/hubble_ugc7983_potw2303a.jpg",
+      description: "yo wuddup 22",
+      price: 998.99,
+      imgDescription: "galaxy 3"
+  },
+  {
+      img: "https://www.nasa.gov/sites/default/files/thumbnails/image/hubble_ugc7983_potw2303a.jpg",
+      description: "yo wuddup 22",
+      price: 998.99,
+      imgDescription: "galaxy 3"
+  }
+];
+export class ProductModel {
+  price: number;
+  imgDescription: string;
+  description: string;
+  img: string;
+
+  constructor(price: number, imgDescription: string, description: string, img: string){
+      this.description =description;
+      this.img =img;
+      this.imgDescription = imgDescription;
+      this.description=description;
+      this.price = price;
+  }
+}
+*/
